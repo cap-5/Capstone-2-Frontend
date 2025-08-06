d01adb9ef77d193b7e9c968387be32e5623fdb24
MS <cg.tyler_774@outlook.com>
2025-08-05 21:41:57 -0400

    fix some the issues in the CreateGroup

--- src/components/CreateGroup.jsx
+++ src/components/CreateGroup.jsx
@@ -1,37 +1,32 @@
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-// const createGroup = (groupInfo) => {
-//   const [createGroup, setCreateGroup] = useState({
-//     description: "",
-//     groupName: "",
-//     Receipt_Id: [],
-//   });
+const createGroup = (groupInfo) => {
+  const [createGroup, setCreateGroup] = useState({
+    description: "",
+    groupName: "",
+    Receipt_Id: [],
+  });
 
-//   const fetchGroupData = async () => {
-//     try {
-//       const res = await axios.post("http://localhost:8080/api/group/create");
-//       const grabInfo = res.data.grabInfo; // this the user info the info that i want and i pass
-//       setCreateGroup({
-//         description: grabInfo.description,
-//         groupName: grabInfo.groupName,
-//         Receipt_Id: grabInfo.Receipt_Id,
-//       });
-//     } catch (err) {
-//       console.error("cant not create new group", err);
-//     }
-//   };
-//   const handleChang = (e) => {
-//     const { name, value } = e.target;
-//     setCreateGroup((prev) => ({
-//       ...prev,
-//       [name]: value,
-//     }));
-//   };
-//   return(<p>welcome {createGroup.groupName}</p>
-// <input 
-
-
-//   );
-// };
-// export default createGroup;
+  const fetchGroupData = async () => {
+    try {
+      const res = await axios.post("http://localhost:8080/api/group/create");
+      const grabInfo = res.data.grabInfo; // this the user info the info that i want and i pass
+      setCreateGroup({
+        description: grabInfo.description,
+        groupName: grabInfo.groupName,
+        Receipt_Id: grabInfo.Receipt_Id,
+      });
+    } catch (err) {
+      console.error("cant not create new group", err);
+    }
+  };
+  const handleChang = (e) => {
+    const { name, value } = e.target;
+    setCreateGroup((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+};
+export default createGroup;
