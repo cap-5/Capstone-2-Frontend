8cb8f4225eabfdac7ee90d41d18b280d4041dca5
Muhammad <khawajat016@gmail.com>
2025-08-12 01:32:45 -0400

    did some css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,31 @@
 import React from "react";
+import { Box, Typography, Button, Container } from "@mui/material";
+import "./Homestyle.css";
 
-const Home = () => {
+const HeroSection = () => {
   return (
-    <>
-      <h1>Hello React!</h1>
-      <img className="react-logo" src="/react-logo.svg" alt="React Logo" />
-    </>
+    <Box className="hero-container">
+      <svg
+        className="topographic-map"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 800 800"
+      >
+        {/* Your SVG content here */}
+      </svg>
+      <Container maxWidth="md" className="content">
+        <Typography variant="h2" className="title">
+          Snap. Split. Send.
+        </Typography>
+        <Typography variant="h6" className="subtitle">
+          Capture your receipts, split the bill, and share with friends
+          instantly.
+        </Typography>
+        <Button variant="contained" size="large" className="cta-button">
+          Get Started
+        </Button>
+      </Container>
+    </Box>
   );
 };
 
-export default Home;
+export default HeroSection;
