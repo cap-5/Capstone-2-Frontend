c887be4af1738954bf157e75ddd36f79570797e5
Bryan Zhong <bryan.zhong46@stu.bmcc.cuny.edu>
2025-08-14 11:21:33 -0400

    Changing groupId in ImageRender to test vercel.

--- src/components/ImageRender.jsx
+++ src/components/ImageRender.jsx
@@ -27,7 +27,7 @@ function OcrComponent() {
   const [isOther, setIsOther] = useState(false);
   const [backendTotal, setBackendTotal] = useState(null);
 
-  const groupId = 4; // Hardcoded for now
+  const groupId = 5; // Hardcoded for now
 
   const predefinedCategories = [
     "Entertainment",
