5bdd2618e74742fb2c9fa87d5adb6cce08058915
Bryan Zhong <bryan.zhong46@stu.bmcc.cuny.edu>
2025-08-15 16:26:13 -0400

    Using tables to display User and Group receipt history.

--- src/components/GroupReceipts.jsx
+++ src/components/GroupReceipts.jsx
@@ -1,6 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import { API_URL } from "../shared";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Container } from "@mui/material";
 
 const GetGroupReceipts = () => {
   const [groupRecpits, setGroupRecpits] = useState([]);
@@ -10,7 +19,7 @@ const GetGroupReceipts = () => {
   const fetchGroupRecpits = async () => {
     try {
       const res = await axios.get(
-        `http://localhost:8080/api/group/GroupReceipts/${groupId}`
+        `${API_URL}/api/group/GroupReceipts/${groupId}`
       );
       console.log(res.data);
       setGroupRecpits(res.data);
@@ -22,14 +31,56 @@ const GetGroupReceipts = () => {
     fetchGroupRecpits();
   }, []);
   console.log("Fetching group receipts", groupRecpits);
+  // return (
+  //   <div>
+  //     <ul>
+  //       {groupRecpits.map((recpit) => {
+  //         return <li key={recpit.id}>title: {recpit.title}</li>;
+  //       })}
+  //     </ul>
+  //   </div>
+  // );
+
   return (
-    <div>
-      <ul>
-        {groupRecpits.map((recpit) => {
-          return <li key={recpit.id}>title: {recpit.title}</li>;
-        })}
-      </ul>
-    </div>
+    <Container maxWidth="md">
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Title</TableCell>
+              <TableCell align="right">Body</TableCell>
+              <TableCell align="right">Category</TableCell>
+              <TableCell align="right">Total</TableCell>
+              <TableCell align="right">Created At:</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {groupRecpits.map((receipt) => (
+              <TableRow
+                key={receipt.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {receipt.title}
+                </TableCell>
+                <TableCell align="right">
+                  {receipt.body.substring(0, 50)}
+                </TableCell>
+                <TableCell align="right">
+                  {receipt.category ? receipt.category : "No category provided"}
+                </TableCell>
+                <TableCell align="right">
+                  ${receipt.totalPay ? receipt.totalPay : 0}
+                </TableCell>
+                <TableCell align="right">
+                  {receipt.createdAt.substring(0, 10)}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
   );
 };
 
--- src/components/UserReceipts.jsx
+++ src/components/UserReceipts.jsx
@@ -2,10 +2,18 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import { API_URL } from "../shared";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Container } from "@mui/material";
 
 const DisplayUserReceipts = () => {
-  const [userReceipts, setUserReceipts] = useState([]); 
-  const {userId} = useParams();
+  const [userReceipts, setUserReceipts] = useState([]);
+  const { userId } = useParams();
 
   const fetchUserReceipts = async () => {
     try {
@@ -21,22 +29,68 @@ const DisplayUserReceipts = () => {
     fetchUserReceipts();
   }, []);
   console.log("this is user", userReceipts);
-  return (
-    <div>
+
+  // return (
+    /* <div className="receipt-container">
       <ul>
-        {userReceipts.map((recpit) => 
-            <li key={recpit.id}>
+        {userReceipts.map((recpit) => (
+          <Container key={recpit.id}>
             <h2>Title:{recpit.title}</h2>
             <ul>
               <li>Body: {recpit.body}</li>
-              <li>Category: {recpit.category ? recpit.category : "No category provided"}</li>
-              <li>Total: ${recpit.totalPay ? recpit.totalPay : 0 }</li>
-              <li>Created at: {recpit.createdAt}</li>
+              <li>
+                Category:{" "}
+                {recpit.category ? recpit.category : "No category provided"}
+              </li>
+              <li>Total: ${recpit.totalPay ? recpit.totalPay : 0}</li>
+              <li>Created at: {recpit.createdAt.substring(0, 10)}</li>
             </ul>
-            </li>
-        )}
+          </Container>
+        ))}
       </ul>
-    </div>
+    </div> */
+
+
+  return (
+    <Container maxWidth="md">
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Title</TableCell>
+              <TableCell align="right">Body</TableCell>
+              <TableCell align="right">Category</TableCell>
+              <TableCell align="right">Total</TableCell>
+              <TableCell align="right">Created At:</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {userReceipts.map((receipt) => (
+              <TableRow
+                key={receipt.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {receipt.title}
+                </TableCell>
+                <TableCell align="right">
+                  {receipt.body.substring(0, 50)}
+                </TableCell>
+                <TableCell align="right">
+                  {receipt.category ? receipt.category : "No category provided"}
+                </TableCell>
+                <TableCell align="right">
+                  ${receipt.totalPay ? receipt.totalPay : 0}
+                </TableCell>
+                <TableCell align="right">
+                  {receipt.createdAt.substring(0, 10)}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
   );
 };
 
