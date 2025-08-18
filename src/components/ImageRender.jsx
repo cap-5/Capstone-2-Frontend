34bb51099dee679d5ce850d958a67568ed808f73
WilliamBoop <williamgsea@gmail.com>
2025-08-18 14:42:36 -0400

    Clickable receipts lead to assign.

--- src/components/ImageRender.jsx
+++ src/components/ImageRender.jsx
@@ -178,7 +178,7 @@ function OcrComponent() {
       );
 
       alert("Receipt saved!");
-      navigate(`/groups/${groupId}`); // go back to group detail
+      navigate(`/groups/${groupId}`); // Good feature maybe?
 
       // Get totalPay directly from saved receipt response
       const savedReceipt = res.data.receipt;
