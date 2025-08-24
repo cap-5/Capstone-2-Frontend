a176a6f5019985ad5c17e57ab0718ec6a5e451de
Jocsan Rodriguez <jasub270@gmail.com>
2025-08-24 13:46:10 -0400

    shared.js

--- src/shared.js
+++ src/shared.js
@@ -1,6 +1,9 @@
-export const API_URL = process.env.REACT_APP_API_URL|| "http://localhost:8080";
-export const SOCKETS_URL = process.env.SOCKETS_URL || "ws://localhost:8080";
+export const API_URL = process.env.API_URL || "http://localhost:8080";
+// export const SOCKETS_URL = process.env.SOCKETS_URL || "ws://localhost:8080";
 export const NODE_ENV = process.env.NODE_ENV || "development";
 
 export const REACT_APP_AUTH0_DOMAIN = "fVH2fH974wnOzz5H8GyAqcdyqYKaN5CI";
 export const REACT_APP_AUTH0_CLIENT_ID = "dev-cfd35oecvakbb3kq.us.auth0.com";
+
+console.log("API_URL:", API_URL);
+console.log("NODE_ENV:", NODE_ENV);
