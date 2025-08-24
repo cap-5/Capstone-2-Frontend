3396246a70dfe98c61c1a95380d1917e19af3ee1
Jocsan Rodriguez <jasub270@gmail.com>
2025-08-24 13:25:55 -0400

    env API URL

--- src/shared.js
+++ src/shared.js
@@ -1,4 +1,4 @@
-export const API_URL = process.env.API_URL || "http://localhost:8080";
+export const API_URL = process.env.REACT_APP_API_URL|| "http://localhost:8080";
 export const SOCKETS_URL = process.env.SOCKETS_URL || "ws://localhost:8080";
 export const NODE_ENV = process.env.NODE_ENV || "development";
 
